020cfd6325765c5f82bf91bdfc16d145bb9ccab2
encarnacao <cencarnacaoq@gmail.com>
2023-07-09 23:55:52 -0300

    feat: create resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { SigninModule } from './signin/signin.module';
+import { PublicationModule } from './publication/publication.module';
+import { PublicationsModule } from './publications/publications.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, SigninModule, PublicationModule, PublicationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
