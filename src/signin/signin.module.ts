b5c6fbf0186cb17559f3bb473b3011aad2b91156
encarnacao <cencarnacaoq@gmail.com>
2023-07-15 18:28:05 -0300

    feat: import jwt and add prisma service to provider

--- src/signin/signin.module.ts
+++ src/signin/signin.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { SigninService } from './signin.service';
 import { SigninController } from './signin.controller';
+import { PrismaService } from 'src/prisma.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+    }),
+  ],
   controllers: [SigninController],
-  providers: [SigninService]
+  providers: [SigninService, PrismaService],
 })
 export class SigninModule {}
