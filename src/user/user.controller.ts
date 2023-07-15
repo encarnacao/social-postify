6bdfbb08256b9c7f1a8de9d45a0f30afcabe31e9
encarnacao <cencarnacaoq@gmail.com>
2023-07-15 18:33:13 -0300

    feat: create register route

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { UserService } from './user.service';
+import { CreateUserDTO } from 'src/entities';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Post()
+  async create(@Body() userData: CreateUserDTO) {
+    const token = await this.userService.create(userData);
+    return { authToken: token };
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,10 +1,22 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { User } from '@prisma/client';
+import { CreateUserDTO } from 'src/entities';
 import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class UserService {
   constructor(private prisma: PrismaService, private jwtService: JwtService) {}
+
+  generateToken(user: User) {
+    const payload = { email: user.email };
+    return this.jwtService.sign(payload);
+  }
+
+  async create(userData: CreateUserDTO){
+    const user = await this.prisma.user.create({ data: userData });
+    return this.generateToken(user);
+  }
 
   async getUserByEmail(email: string) {
     return await this.prisma.user.findUnique({ where: { email } });
