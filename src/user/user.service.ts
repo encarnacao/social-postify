a343062feb4a651ea4185243fd465b40b82051a0
encarnacao <cencarnacaoq@gmail.com>
2023-07-15 19:02:48 -0300

    feat: add hashing

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,12 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { AuthService } from 'src/auth/auth.service';
 import { CreateUserDTO } from 'src/entities';
 import { PrismaService } from 'src/prisma.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
   constructor(private prisma: PrismaService, private authService: AuthService) {}
 
   async create(userData: CreateUserDTO){
+    userData.password = await bcrypt.hash(userData.password, 10);
     const user = await this.prisma.user.create({ data: userData });
     return this.authService.generateToken(user);
   }
